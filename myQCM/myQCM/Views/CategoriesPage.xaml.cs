549e70ee4003ab4202e1f4368d9410bdb1d16fe4
alexislepage <alexislepage@hotmail.fr>
2016-03-16 10:24:32 +0100

    Add Model and View Category.

---
+++ myQCM/myQCM/Views/CategoriesPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace myQCM.Views
+{
+    public partial class CategoriesPage : PhoneApplicationPage
+    {
+        public CategoriesPage()
+        {
+#if DEBUG
+            this.DataContext = new ViewModels.ViewModelCategories();
+#else
+
+#endif
+            InitializeComponent();
+        }
+    }
+}
